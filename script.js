5af59658f63097eac7def94ea2dd9ac49eabe903
PauloLucas <paulolucas88@gmail.com>
2024-02-13 14:16:01 -0300

    remocao comentarios

--- script.js
+++ script.js
@@ -13,27 +13,22 @@ document
       return;
     }
 
-    // Aqui você pode adicionar lógica de autenticação
-    // ...
-
-    // Exemplo de verificação simples (substitua por lógica real de autenticação)
     if (username === "usuario@email.com" && password === "senha") {
       alert("Login bem-sucedido!");
-      // Adicione redirecionamento ou outras ações após o login bem-sucedido
+      //outras ações após o login bem-sucedido
     } else {
       alert("Login falhou. Verifique seu email e senha.");
     }
   });
 
-// Função para validar se o "username" é um email válido
+//validar se o "username" é um email válido
 function isValidEmail(email) {
   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
 
-// Adicione este trecho ao seu arquivo script.js ou inclua no script existente
+// script.js ou inclua no script existente
 function onSignInCustom() {
-  // Lógica personalizada para processar o login com o botão personalizado
   console.log("Processando login com botão personalizado");
 }
 
@@ -42,5 +37,4 @@ function onSignIn(googleUser) {
   console.log("ID: " + profile.getId()); // ID do Google
   console.log("Nome: " + profile.getName()); // Nome completo
   console.log("E-mail: " + profile.getEmail()); // E-mail
-  // Adicione lógica para autenticar o usuário no seu sistema
 }
